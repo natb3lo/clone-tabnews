3b29f6bdab83cd3b93ddd2e80153adf962986dd3
Natã Belo <nbsantosv@gmail.com>
2025-10-10 11:42:44 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,30 @@
 function Home() {
+  return (
+    <div>
+      <h1>Bleh💨💨</h1>
+      <style jsx>{`
+        div {
+          height: 100vh;
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
 
-    return (
-        
-        <div>
-        <h1>Bleh💨💨
-        </h1>
-        <style jsx>{`
-            div{
-                height: 100vh;
-                width: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                text-align: center;
-
-                background-image:
-                url('/bleh.jpg');
-                background-size: cover;     
-                background-position: center;  
-                background-repeat: no-repeat;
-            }
-            h1{
-                text-align: center;
-                color: white;
-                font-size: 6rem;
-                text-shadow: 2px 2px 6px rgba(0,0,0,0.5);
-                margin: 0;
-                
-            }
-            `}</style>
-        
-        </div>
-
-    )
-
+          background-image: url("/bleh.jpg");
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        h1 {
+          text-align: center;
+          color: white;
+          font-size: 6rem;
+          text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.5);
+          margin: 0;
+        }
+      `}</style>
+    </div>
+  );
 }
-
-export default Home;  
-
+export default Home;

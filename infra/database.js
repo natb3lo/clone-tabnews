4af21695dcf6d1ba34bbbab2efe0b5d3ee1afdb5
Natã Belo <nbsantosv@gmail.com>
2025-10-18 19:14:16 -0300

    Deixa o `database.js` `client.connect()` mais robusto a erros

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,23 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
+
+  const postresCredentials = {
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+  };
+  console.log("Postgres credentials: ", postresCredentials);
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result.rows;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }

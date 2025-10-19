1496c7ca13318908cb8173ea59f571d85638a143
Natã Belo <nbsantosv@gmail.com>
2025-10-19 12:49:07 -0300

    Adiciona scripts de migrations

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLSection(),
   });
 
   try {
@@ -20,6 +20,16 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLSection() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {

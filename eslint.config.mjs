2e204348d52acdf998f3a21f372cf50feaa16ecf
Natã Belo <nbsantosv@gmail.com>
2025-11-06 21:08:30 -0300

    Adiciona script `lint:eslint:checl` do eslint

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-//import js from "@eslint/js";
-
-export default defineConfig([
-  ...nextVitals,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-]);
